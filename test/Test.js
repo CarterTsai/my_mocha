547ed008edf98ebf3545ec592192aca856692919
Carter Tsai <hamming1@gmail.com>
2013-08-08 23:53:37 +0800

    Mocha unit testing references: http://brianstoner.com/blog/testing-in-nodejs-with-mocha/

---
+++ test/Test.js
@@ -0,0 +1,15 @@
+var assert = require("assert");
+
+describe('Test mocha ',function() {
+    it('should equal test',function() {
+        assert.equal( 'test', 'test' ); 
+    });
+
+    it('should equal test',function() {
+        assert.equal( 'test', 'test' ); 
+    });
+    
+    it('should equal test',function() {
+        assert.equal( 'test', 'test' ); 
+    })
+})
